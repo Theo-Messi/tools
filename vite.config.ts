3965cf9937e636d99b97cf060b9cfbd43a6bc33b
Theo <fanxiaobin422@gmail.com>
2024-08-02 13:42:50 +0800

    chore: Add and configure formatting tools
    chore: add release ci

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: './vue/index.ts',
+      name: 'tmfe',
+      fileName: (format) => `tmfe.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue', 'vitepress'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          vitepress: 'VitePress'
+        }
+      }
+    }
+  }
+});
