266f4967b86797f75b2abf57f05ec284d147d543
Theo <fanxiaobin422@gmail.com>
2024-10-25 14:22:00 +0800

    feat(solis): add image style (#97)

--- packages/Solis/types/theme.ts
+++ packages/Solis/types/theme.ts
@@ -5,7 +5,7 @@ export { default as Page } from '../components/Page.vue'
 export { default as Twikoo } from '../components/Twikoo.vue'
 export { default as Info } from '../components/DocInfo.vue'
 
-import '../style/styles.scss'
+import '../style/index.scss'
 
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
